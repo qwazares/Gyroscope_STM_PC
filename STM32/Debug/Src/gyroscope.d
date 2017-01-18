c74adf62da6ce3ebdff50ffadac6aff21c4cdf97
qwazares <kkopec1993@gmail.com>
2017-01-18 15:01:09 +0100

    Add files via upload

---
+++ STM32/Debug/Src/gyroscope.d
@@ -0,0 +1,91 @@
+Src/gyroscope.o: ../Src/gyroscope.c \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/gyroscope.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/mxconstants.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/gyroscope.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Inc/mxconstants.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/Users/Kris/Documents/STM32\ Projects/gyro/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
